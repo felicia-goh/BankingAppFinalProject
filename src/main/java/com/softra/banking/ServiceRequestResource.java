567e1fd0c8cd8ce46c00cf876403575302a57078
Hiew Jin Nee <hiewjinneeeee@gmail.com>
2021-09-13 15:15:00 +0800

    Added @CrossOrigin("*") for all controllers
    This is to facilitate the resource sharing between FE and BE

--- src/main/java/com/softra/banking/ServiceRequestResource.java
+++ src/main/java/com/softra/banking/ServiceRequestResource.java
@@ -2,6 +2,7 @@ package com.softra.banking;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class ServiceRequestResource  {
   
   @Autowired
